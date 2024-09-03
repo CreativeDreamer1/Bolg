f3dc99086bc4966c30fc9c826819353bc1779215
CreativeDreamer1 <mdawoodnasaryab@gmail.com>
2024-09-04 00:13:45 +0500

    second commit

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# A Model that stores all the information in database
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, label="Title")
+    description = models.TextField()
+    image = models.ImageField(upload_to="products")
+    created_at = models.TimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def edit(self, name, description, image):
+        self.name = name
+        self.description = description
+        self.image = image
+        self.save()
+    
+    def shortDescription(self):
+        words = self.description.split()
+        if len(words) > 50:
+            return " ".join(words[:30]) + "..."
+        else:
+            return self.description
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# This will show the list of all available blogs
+
+
+def productList(req):
+    return render(req, 'gallery/productList.html')
